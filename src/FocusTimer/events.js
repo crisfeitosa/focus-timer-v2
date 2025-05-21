82f022d9e88ca0a8908d815efafb0b34557df34e
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-20 22:08:48 -0300

    Finalizando a escolha dos minutos do temporizador

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,6 +1,8 @@
+import state from './state.js';
 import { controls } from './elements.js';
 import * as actions from './actions.js';
 import * as el from './elements.js';
+import { updateDisplay } from './timer.js';
 
 export function registerControls() {
   controls.addEventListener('click', (event) => {
@@ -20,5 +22,17 @@ export function setMinutes() {
 
   el.minutes.addEventListener('keydown', (event) => {
     /\d/.test(event.key) ? null : event.preventDefault();
+  });
+
+  el.minutes.addEventListener('blur', (event) => {
+    let time = event.currentTarget.textContent;
+
+    time = time > 60 ? 60 : time;
+
+    state.minutes = time;
+    state.seconds = 0;
+
+    updateDisplay();
+    el.minutes.removeAttribute('contenteditable');
   });
 };
